b35d186c402e49a1da872d029fd82faa0f7636a0
Rushi.Bhavitha <you@example.comRushiBhavitha>
2026-06-30 22:00:46 +0530

    feature/oauth-callback

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
         changeOrigin: true,
         secure: false,
       },
-      '/auth': {
+      '/auth/google': {
         target: 'http://localhost:5000',
         changeOrigin: true,
         secure: false,
